b8940d585500b7ed9895ae0976cea9c907fcba28
Kelly Dwan <kelly.dwan@gmail.com>
2014-04-05 15:22:22 -0400

    Add the header & footer menus

--- footer.php
+++ footer.php
@@ -11,12 +11,17 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'mfcs_2014' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'mfcs_2014' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'mfcs_2014' ), 'Mill Falls GiveCamp 2014', '<a href="http://themes.redradar.net" rel="designer">GiveCamp (Kelly Dwan & Mel Choyce)</a>' ); ?>
-		</div><!-- .site-info -->
+
+		<nav id="footer-navigation" class="main-navigation" role="navigation">
+			<?php wp_nav_menu( array( 'theme_location' => 'footer' ) ); ?>
+		</nav><!-- #footer-navigation -->
+
 	</footer><!-- #colophon -->
+
+	<div class="site-info">
+		Copyright 2014 Mill Falls Charter School
+	</div><!-- .site-info -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -24,8 +24,13 @@
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
+
+		<nav id="header-navigation" class="main-navigation" role="navigation">
+			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'mfcs_2014' ); ?></a>
+
+			<?php wp_nav_menu( array( 'theme_location' => 'header' ) ); ?>
+		</nav><!-- #header-navigation -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'mfcs_2014' ); ?></h1>
