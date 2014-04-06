df7896da226c4bccbd33187fcada3838e6c646b5
Kelly Dwan <kelly.dwan@gmail.com>
2014-04-05 21:20:23 -0400

    Style interior page headers

--- page.php
+++ page.php
@@ -21,25 +21,32 @@ get_header(); ?>
 	</div>
 
 	<div class="container">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		<header class="entry-header">
+			<h1 class="entry-title"><?php the_title(); ?></h1>
+		</header><!-- .entry-header -->
+
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
 
-				<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
 
-					<?php get_template_part( 'content', 'page' ); ?>
-
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template
-						if ( comments_open() || '0' != get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
-
-				<?php endwhile; // end of the loop. ?>
+				<?php
+					// If comments are open or we have at least one comment, load up the comment template
+					if ( comments_open() || '0' != get_comments_number() ) :
+						comments_template();
+					endif;
+				?>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
+
+		<?php endwhile; // end of the loop. ?>
+
+		<?php get_sidebar(); ?>
+
 	</div>
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
