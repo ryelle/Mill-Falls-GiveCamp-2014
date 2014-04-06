0d5ba2ce1b89908195b7fa15bb185b27fbcca9c2
Kelly Dwan <kelly.dwan@gmail.com>
2014-04-06 12:10:45 -0400

    Add HTML5 Shiv for IE

--- functions.php
+++ functions.php
@@ -100,6 +100,8 @@ add_action( 'widgets_init', 'mfcs_2014_widgets_init' );
  * Enqueue scripts and styles.
  */
 function mfcs_2014_scripts() {
+	global $wp_scripts; // declared so we can add data, conditionals, to styles.
+
 	wp_enqueue_style( 'mfcs_2014-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'mfcs_2014-navigation', get_template_directory_uri() . '/js/tiny-nav.js', array('jquery'), '20120206', true );
